c39a61117469e1184643ed7ef7b80ed5b4b6f23f
jpileto <jp.ileto@icloud.com>
2017-04-09 17:07:10 -0400

    refactor the total_funds & interest_time class methods

--- bank.rb
+++ bank.rb
@@ -5,11 +5,13 @@ class BankAccount
 
   attr_accessor :balance
 
-  def initialize
-    @name = name
+  def initialize(account)
     @balance = 0
-
   end
+
+  # def balance
+  #   @balance
+  # end
 
   def deposit(money)
     @balance += money
@@ -20,21 +22,40 @@ class BankAccount
   end
 
   def self.create
-    @@accounts [] = BankAccount.new
-    @balance = 0
+    account = BankAccount.new(account)
+    @@accounts << account
+    return account
   end
 
   def self.total_funds
-    @@accounts.reduce(:+)
+    # @@accounts.reduce(:+)
+    sum = 0
+    @@accounts.each do |account|
+      sum = sum += account.balance
+    end
   end
 
-  def interest_time
-    @@accounts.each do |a|
-      a = (1 + @@interest_rate) * a
+  def self.interest_time
+    @@accounts.each do |account|
+      account.balance += account.balance * @@interest_rate
     end
   end
 end
 
 
-puts my_account = BankAccount.create
-# puts my_account = BankAccount.new
+my_account = BankAccount.create
+your_account = BankAccount.create
+puts my_account.balance # 0
+puts BankAccount.total_funds # 0
+my_account.deposit(200)
+your_account.deposit(1000)
+puts my_account.balance # 200
+puts your_account.balance # 1000
+puts BankAccount.total_funds # 1200
+BankAccount.interest_time
+puts my_account.balance # 202.0
+puts your_account.balance # 1010.0
+puts BankAccount.total_funds # 1212.0
+my_account.withdraw(50)
+puts my_account.balance # 152.0
+puts BankAccount.total_funds # 1162.0
